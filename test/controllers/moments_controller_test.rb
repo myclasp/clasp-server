d1462adc8da465c17272591b616ff5abb17f7f87
Roryg <giannichan@gmail.com>
2016-09-18 19:59:07 +0100

    adds moment creation test

--- test/controllers/moments_controller_test.rb
+++ test/controllers/moments_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MomentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should create moments" do
+
+    new_moments = [{ timestamp: Time.now, state: 'up', latitude: 12.00, longitude: -12.00 }]
+    
+    expected = Moment.count+new_moments.size
+
+    post '/moments',
+      params: new_moments.to_json,
+      headers: { 'Accept' => Mime[:json], 'Content-Type' => Mime[:json].to_s },
+      env: {},
+      xhr: false,
+      as: :json
+
+    assert_response :success
+    assert_equal "application/json", response.content_type
+    assert_equal expected, Moment.count
+  end
+
 end
