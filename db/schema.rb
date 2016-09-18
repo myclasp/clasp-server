c138fa6840dc067920a88efb44d7236d478d31fe
Roryg <giannichan@gmail.com>
2016-09-18 17:29:36 +0100

    migrate moments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160918162010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "moments", force: :cascade do |t|
+    t.string   "state"
+    t.datetime "timestamp"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
