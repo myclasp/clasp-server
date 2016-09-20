c306e7b100a70d78c03301f2d17d6f67835ff7ac
Roryg <giannichan@gmail.com>
2016-09-20 15:53:43 +0100

    adds format error handling and new format for moments

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -1,14 +1,44 @@
+class MomentsFormatError < StandardError
+  attr_accessor :message
+  def initialize(message=nil)
+    @message = message
+  end
+end
+
 class MomentsController < ApplicationController
+
+  rescue_from MomentsFormatError, :with => :rescue_from_moments_format_error
 
   def create
     moments = nil
     moments = JSON.parse(params[:moments]) unless params[:moments].blank?
-
-    results = []
-    moments.each { |m| results << Moment.create(m).valid? } unless moments.blank?
     
-    success = !(results.include?(false))
-    render json: { success: success, results: results }
+    raise MomentsFormatError.new("Moments should be a collection.") unless moments.class.eql?(Array)
+    
+    passed = []
+    failed = []
+    errors = {}
+
+    moments.each do |moment|
+      m = Moment.create(moment)
+      raise MomentsFormatError.new("Blank identifier for moment.") if m.identifier.blank?
+      
+      if m.valid?
+        passed << m.identifier
+      else
+        failed << m.identifier
+        errors.merge!({ m.identifier => m.errors.full_messages.join(". ") }) 
+      end
+    end
+
+    success = failed.blank?
+    render json: { success: success, passed: passed, failed: failed, errors: errors }    
+  end
+
+  private
+
+  def rescue_from_moments_format_error(e)
+    render :status => 400, json: { success: false, message: "Request doesn't meet required format: " + e.message }
   end
 
 end
--- test/controllers/moments_controller_test.rb
+++ test/controllers/moments_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MomentsControllerTest < ActionDispatch::IntegrationTest
   
   test "should create moments" do
-    new_moments = [{ timestamp: Time.now, state: 'up', latitude: 12.00, longitude: -12.00 }]
+    new_moments = [{ identifier:1, timestamp: Time.now, state: 0, latitude: 12.00, longitude: -12.00 }]
     expected = Moment.count+new_moments.size
 
     post '/moments',
@@ -16,6 +16,32 @@ class MomentsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal "application/json", response.content_type
     assert_equal expected, Moment.count
+  end
+
+  test "should respond with 400 error for format" do
+    post '/moments',
+      params: { moments: { timestamp: Time.now }.to_json },
+      headers: { 'Accept' => Mime[:json], 'Content-Type' => Mime[:json].to_s },
+      env: {},
+      xhr: false,
+      as: :json
+
+    assert_response(400)
+    assert_equal false, JSON.parse(response.body)["success"]
+    assert_equal "application/json", response.content_type
+  end
+
+  test "should respond with 400 error for missing identifier" do
+    post '/moments',
+      params: { moments: [{ timestamp: Time.now }].to_json },
+      headers: { 'Accept' => Mime[:json], 'Content-Type' => Mime[:json].to_s },
+      env: {},
+      xhr: false,
+      as: :json
+
+    assert_response(400)
+    assert_equal false, JSON.parse(response.body)["success"]
+    assert_equal "application/json", response.content_type
   end
 
 end
