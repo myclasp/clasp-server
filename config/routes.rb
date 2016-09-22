386e03b23af5396658c0f2f3344eeff939111e15
Roryg <giannichan@gmail.com>
2016-09-22 18:18:10 +0100

    root to home

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   post 'auth_user', to: 'authentication#authenticate_user'
-  get 'pages/home'
+  root to: 'pages#home'
   post 'moments', to: 'moments#create', as: :moments
   
 end
