ad98c376cd48c884d07c6e28641ece789eb9819a
Roryg <giannichan@gmail.com>
2016-09-22 17:08:41 +0100

    adds user uuid for external use

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :set_uuid
+
+  def set_uuid
+    self.uuid = Digest::SHA1.hexdigest(User.count.to_s + Time.now.to_s)[0..9] if self.uuid.blank?
+  end
+
 end
