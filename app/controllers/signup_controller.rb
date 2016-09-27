bb4f677ef384e951cb8ba770286c8da445c1f51f
Roryg <giannichan@gmail.com>
2016-09-27 12:07:48 +0100

    adds sign up controller

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,22 @@
+class SignupController < ApplicationController
+
+  protect_from_forgery :except => [:create_user]
+  respond_to :json
+
+  def create_user
+    user = User.new(user_params)
+
+    if user.save
+      render :json => { success:true, email: user.email, id: user.uuid }, status: 201
+      return
+    else
+      warden.custom_failure!
+      render :json => { success:false, errors: user.errors }, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
