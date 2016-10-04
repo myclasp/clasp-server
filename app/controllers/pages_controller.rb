b33c3a22a30bc19622ea17290a221a039faf87a0
Roryg <giannichan@gmail.com>
2016-10-04 17:51:03 +0100

    adds groups to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+      @groups = Group.all
+    else
+      @groups = Group.where(is_private: false).all
+    end
   end
 end
