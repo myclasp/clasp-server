f3d92113e822bbfee267106aa52c78bc19b02c00
Roryg <giannichan@gmail.com>
2016-10-04 13:25:31 +0100

    adds default values for preferences

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,18 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
   serialize :preferences, Hash
-  
+
+  def preferences
+    prefs = read_attribute(:preferences)
+    
+    default = { visualisations: [:line_graph, :map, :calendar, :bar], 
+      private: false, data_available: false }
+    
+    default.each do |key,value|
+      prefs[key] = value if prefs[key].nil?
+    end
+    
+    return prefs
+  end
+
 end
