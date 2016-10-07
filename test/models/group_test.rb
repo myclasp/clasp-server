1a8ec4539bb1aa430790a154e213777b55012bf3
Roryg <giannichan@gmail.com>
2016-10-07 13:04:26 +0100

    setup fixtures and test for group moments selector

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -5,4 +5,11 @@ class GroupTest < ActiveSupport::TestCase
     g = Group.new 
     assert g.preferences.eql?(Group.default_preferences)
   end
+
+  test "#moments returns all moments belonging to members" do
+    g = Group.first
+    moments_total = 0
+    User.all.each { |u| moments_total+=u.moments.count; g.users << u }
+    assert g.moments.count.eql?(moments_total)
+  end
 end
