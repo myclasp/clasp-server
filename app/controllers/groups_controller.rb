71de901a4c01a8b08d99e7e694ff077464a885c9
Roryg <giannichan@gmail.com>
2016-10-11 13:24:25 +0100

    adds moment details to markers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,16 @@ class GroupsController < ApplicationController
 
     for moment in @group.moments
       next unless moment.has_location?
+
+      popup = "latitude: " + moment.latitude.to_s  +
+        ", longitude: "  + moment.longitude.to_s +
+        ", identifier: " + moment.identifier.to_s+
+        ", state: " +      moment.state.to_s  +
+        ", timestamp: " +  moment.timestamp.to_s 
+      
       @markers.push({
-        :latlng => [moment.latitude, moment.longitude],
-        :popup => "Hello!"
+        latlng: [moment.latitude, moment.longitude],
+        popup: popup
       })
     end
 
