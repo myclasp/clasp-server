4ff0bb7e26b69271742a1dd22a78f2a341c7b92c
Roryg <giannichan@gmail.com>
2016-10-15 19:29:20 +0100

    adds image url to groups

---
+++ db/migrate/20161015182349_add_image_url_to_groups.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToGroups < ActiveRecord::Migration[5.0]
+  def change
+    add_column :groups, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004164801) do
+ActiveRecord::Schema.define(version: 20161015182349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161004164801) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "is_private",  default: false
+    t.string   "image_url"
   end
 
   create_table "memberships", force: :cascade do |t|
