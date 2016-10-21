31287f5fff75c595505552cb03562bececc86e9e
Roryg <giannichan@gmail.com>
2016-10-21 18:33:23 +0100

    refactor popup

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,16 +8,10 @@ class GroupsController < ApplicationController
     for moment in @group.moments
       next unless moment.has_location?
 
-      popup = "latitude: " + moment.latitude.to_s +
-        ", longitude: "  + moment.longitude.to_s +
-        " identifier: " + moment.identifier.to_s+
-        " state: " +      moment.state.to_s  +
-        " timestamp: " +  moment.timestamp.to_s
-
       @markers.push({
+        moment: moment,
         latlng: [moment.latitude, moment.longitude],
         state: moment.state,
-        popup: popup.html_safe, 
         is_mine: moment.user.eql?(current_user)
       })
     end
